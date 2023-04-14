0677acb0f0646f673ed6247e40454d1c091f3236
Mahabub Alam <rjmahabub543@gmail.com>
2023-04-14 12:00:44 +0600

    modify

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ async function Products(){
         const result  = await cursor.toArray()
         res.send(result)
       })
+      app.get('/menus', async(req, res)=>{
+        const query = {};
+        const cursor  = await ProductsCollection.find(query)
+        const result  = await cursor.toArray()
+        res.send(result)
+      })
+      
     //   app.delete('/categories/:id', async(req, res)=>{
     //     const id = req.params.id;
     //     const query = {_id: ObjectId(id)}
